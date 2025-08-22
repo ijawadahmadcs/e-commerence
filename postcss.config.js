68dbf76faec7f5f2da589e560347d7bc31318001
ijawadahmadcs <ijawadahmadcs@gmail.com>
2025-08-22 12:59:58 +0500

    env-fixed

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,5 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {},
-  },
-   plugins: {
+  plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
